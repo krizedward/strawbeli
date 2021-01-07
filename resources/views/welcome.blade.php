34d5612872609f97cbb252ba4d659416ae009a02
Ronald7901 <ronald.kusuma@binus.ac.id>
2021-01-07 12:26:21 +0700

    update: folder di view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,17 +81,15 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Strawbeli
                 </div>
-
+                
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#">360 Gallery</a>
+                    <a href="#">Destination</a>
+                    <a href="#">Cultivation</a>
+                    <a href="#">Shop</a>
+                    <a href="#">Forum</a>
                 </div>
             </div>
         </div>
